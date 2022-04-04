ddfe0b5c66ab6e0cdb73c85eb028a75dd9f45654
Orooo <86406278+Orooo@users.noreply.github.com>
2022-04-04 23:59:18 +0900

    Add files via upload

---
+++ Covid_Shape/Covid_Shape/covid_in_turtle.py
@@ -0,0 +1,31 @@
+# [Title] Covid shape in console with turtle library
+
+import turtle
+
+Cppsecrets = turtle.Turtle()
+
+s = turtle.screensize(1400, 1400)
+Cppsecrets.pencolor("red")
+Cppsecrets.write("Stay Home", 'false', 'center', font=('Showcard gothic', 40))
+
+s.bgcolor("black")
+Cppsecrets.pencolor("dark green")
+Cppsecrets.pensize(2)
+Python=0
+CplusPlus=0
+
+Cppsecrets.speed(0)
+Cppsecrets.penup()
+Cppsecrets.goto(0,560)
+Cppsecrets.pendown()
+while True:
+    Cppsecrets.forward(Python)
+    Cppsecrets.right(CplusPlus)
+    Python+=3
+    CplusPlus+=1
+    if CplusPlus==210:
+        break
+    Cppsecrets.hideturtle()
+turtle.done()
+
+
